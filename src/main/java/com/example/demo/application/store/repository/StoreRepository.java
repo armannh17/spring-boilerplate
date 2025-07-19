d2b6b932f496e6526a18a966b5cc36f1b01a7686
arman <armannh17@gmail.com>
2025-07-19 12:56:18 +0330

    remove unused code

--- src/main/java/com/example/demo/application/store/repository/StoreRepository.java
+++ src/main/java/com/example/demo/application/store/repository/StoreRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.application.store.dao.StoreDao;
 import com.example.demo.application.store.entity.StoreEntity;
 import com.example.demo.application.store.mapper.StoreMapper;
 import com.example.demo.application.store.model.StoreModel;
-import jakarta.persistence.EntityManager;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.stereotype.Repository;
@@ -13,12 +12,10 @@ import org.springframework.stereotype.Repository;
 public class StoreRepository {
   private final StoreDao storeDao;
   private final StoreMapper storeMapper;
-  private final EntityManager em;
 
-  public StoreRepository(StoreDao storeDao, StoreMapper storeMapper, EntityManager em) {
+  public StoreRepository(StoreDao storeDao, StoreMapper storeMapper) {
     this.storeDao = storeDao;
     this.storeMapper = storeMapper;
-    this.em = em;
   }
 
   public Optional<StoreModel> findById(UUID id) {
